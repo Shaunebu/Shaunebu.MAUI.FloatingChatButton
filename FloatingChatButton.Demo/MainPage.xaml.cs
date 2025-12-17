2ea8491aa77ec282e74c858339bc1ac5cceb3238
MykhailoDav <davudenko.mukhailo@gmail.com>
2025-12-17 22:03:12 +0200

    Add Send on Enter toggle and message command binding on sample app.

--- FloatingChatButton.Demo/MainPage.xaml.cs
+++ FloatingChatButton.Demo/MainPage.xaml.cs
@@ -1,25 +1,45 @@
-﻿namespace FloatingChatButton.Demo
+﻿using System.Windows.Input;
+
+namespace FloatingChatButton.Demo
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
 
+        public ICommand MessageSentCommand { get; }
+
         public MainPage()
         {
             try
             {
+                MessageSentCommand = new Command<string>(OnMessageSent);
+
                 InitializeComponent();
-                myfloatingButton.Messages = new System.Collections.ObjectModel.ObservableCollection<FloatingChatButton.Models.ChatMessage>
-            {
-                new FloatingChatButton.Models.ChatMessage { Text = "Hello, how can I help you?", IsIncoming = true },
-                new FloatingChatButton.Models.ChatMessage { Text = "I have a question about your product.", IsIncoming = false },
-                new FloatingChatButton.Models.ChatMessage { Text = "Sure! What would you like to know?", IsIncoming = true }
-            };
+
+                BindingContext = this;
+
+                myfloatingButton.Messages =
+                [
+                    new() { Text = "Hello, how can I help you?", IsIncoming = true },
+                    new() { Text = "I have a question about your product.", IsIncoming = false },
+                    new() { Text = "Sure! What would you like to know?", IsIncoming = true }
+                ];
             }
             catch (Exception ex)
             {
 
             }
+        }
+
+        private async void OnMessageSent(string message)
+        {
+            await Task.Delay(1000);
+
+            myfloatingButton.Messages.Add(new()
+            {
+                Text = $"You said: {message}",
+                IsIncoming = true
+            });
         }
 
         private void OnCounterClicked(object? sender, EventArgs e)
